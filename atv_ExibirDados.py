f44036d71c8c3e3ecfb1afcbd7967133e44a86e3
Tiago Pitombeira <168240874+pitombeira1984@users.noreply.github.com>
2024-09-19 00:08:42 -0300

    Add files via upload

---
+++ atv_ExibirDados.py
@@ -0,0 +1,4 @@
+nome = str(input('Digite seu nome:'))
+idade = int(input('Digite sua idade:'))
+cidade = str(input('Digite sua cidade:'))
+print(f'{nome},  {idade} anos, morador(a) de {cidade}.')
---
+++ atv_HoraTrabalho.py
@@ -0,0 +1,4 @@
+Rhora = float(input('Quanto vc ganha por hora:'))
+Nhora = float(input('Numero de horas trabalhada no mes:'))
+salario = (Rhora*Nhora)
+print(f'O total do seu salario no mes = R${salario}')
